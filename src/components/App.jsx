1ab89bdf89a78ca968be6ba3337c2b2f457a2030
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-05-21 19:22:38 +0200

    Add ErrorBoundary for error handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import Page from "components/Page/Page.styled";
 import Header from "components/Header/Header.styled";
 import { LocalStorageIcon } from "./Icons/LocalStorageIcon";
 import Phonebook from "components/Phonebook/Phonebook";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
 export const App = () => {
   return (
@@ -10,7 +11,9 @@ export const App = () => {
         Contact Book
         <LocalStorageIcon fill="white" width="72px" height="72px" />
       </Header>
-      <Phonebook />
+      <ErrorBoundary>
+        <Phonebook />
+      </ErrorBoundary>
     </Page>
   );
 };
